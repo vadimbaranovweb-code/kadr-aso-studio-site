320588876738ab4e41f2072962245a6d9aab3bee
Vadim Baranov <vadimbaranovweb@icloud.com>
2026-09-14 16:18:19 +0300

    Publish KADR 0.7.1 from 4e1a794

--- mvp/model.js
+++ mvp/model.js
@@ -1,6 +1,6 @@
 import {validateMarks,legacyMarks} from './text-marks.js';
 import {ANDROID_DEVICES,IPHONE_DEVICES,DEFAULT_ANDROID_DEVICE} from './devices.js';
-export const VERSION='0.7.0';
+export const VERSION='0.7.1';
 export const STORES={
   apple:{name:'App Store',width:1320,height:2868,phone:'iPhone',min:1,max:10},
   android:{name:'Android · RuStore / Google Play',width:1080,height:1920,phone:'Android',min:1,max:10}
